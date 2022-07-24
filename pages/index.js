c2c0a662ced6e38be2cb72e1b5dafaf730c2b8a6
Restyled.io <commits@restyled.io>
2022-07-24 03:28:33 +0000

    Restyled by clang-format

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 export default function Home() {
-  return (
-    <>
-      <h1 className='underline'>Hello world!</h1>
-    </>
-  );
+  return (<><h1 className = 'underline'>Hello world!</h1>
+    </>);
 }
