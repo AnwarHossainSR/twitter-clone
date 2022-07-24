285a9f9c196804f3b31c39a8a18e128a7e4e02cd
AnwarHossainSR <mahedianwar@gmail.com>
2022-07-24 17:16:32 +0600

    design feed section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Feed from '../components/Feed';
 import Sidebar from '../components/Sidebar';
 
 export default function Home({ trendingResults, followResults, providers }) {
@@ -13,6 +14,7 @@ export default function Home({ trendingResults, followResults, providers }) {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
+        <Feed />
         {/* Widgets */}
 
         {/* Modal */}
