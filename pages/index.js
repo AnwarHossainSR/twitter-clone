f6d0efd87bb9fcf4806be76cf58830befd4fb396
AnwarHossainSR <mahedianwar@gmail.com>
2022-07-24 16:51:56 +0600

    design sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-export default function Home() {
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
+
+export default function Home({ trendingResults, followResults, providers }) {
   return (
-    <>
-      <h1 className='underline'>Hello world!</h1>
-    </>
+    <div className=''>
+      <Head>
+        <title>Home / Twitter</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className='bg-black min-h-screen flex max-w-auto mx-auto'>
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
+    </div>
   );
 }
